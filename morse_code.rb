ae45f5121390fcee8ad740e44cc60c1e6404e6de
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-18 16:43:09 +0100

    Create decode_word() method

--- morse_code.rb
+++ morse_code.rb
@@ -1,58 +1,74 @@
+# decode one character
 def decode_char(char)
-    if char == '.-'
-        "A"
-    elsif char == '-...'
-        "B"
-    elsif char == '-.-.'
-        "C"
-    elsif char == '-..'
-        "D"
-    elsif char == '.'
-        "E"
-    elsif char == '..-.'
-        "F"
-    elsif char == '--.'
-        "G"
-    elsif char == '....'
-        "H"
-    elsif char == '..'
-        "I"
-    elsif char == '.---'
-        "J"
-    elsif char == '-.-'
-        "K"
-    elsif char == '.-..'
-        "L"
-    elsif char == '--'
-        "M"
-    elsif char == '-.'
-        "N"
-    elsif char == '---'
-        "O"
-    elsif char == '.--.'
-        "P"
-    elsif char == '--.-'
-        "Q"
-    elsif char == '.-.'
-        "R"
-    elsif char == '...'
-        "S"
-    elsif char == '-'
-        "T"
-    elsif char == '..-'
-        "U"
-    elsif char == '...-'
-        "V"
-    elsif char == '.--'
-        "W"
-    elsif char == '-..-'
-        "X"
-    elsif char == '-.--'
-        "Y"
-    elsif char == '--..'
-        "Z"
-    else
-        "Invalide morse code"
-    end
-    end
-    puts(decode_char(".-"))
+  if char == '.-'
+    "A"
+  elsif char == '-...'
+    "B"
+  elsif char == '-.-.'
+    "C"
+  elsif char == '-..'
+    "D"
+  elsif char == '.'
+    "E"
+  elsif char == '..-.'
+    "F"
+  elsif char == '--.'
+    "G"
+  elsif char == '....'
+    "H"
+  elsif char == '..'
+    "I"
+  elsif char == '.---'
+    "J"
+  elsif char == '-.-'
+    "K"
+  elsif char == '.-..'
+    "L"
+  elsif char == '--'
+    "M"
+  elsif char == '-.'
+    "N"
+  elsif char == '---'
+    "O"
+  elsif char == '.--.'
+    "P"
+  elsif char == '--.-'
+    "Q"
+  elsif char == '.-.'
+    "R"
+  elsif char == '...'
+    "S"
+  elsif char == '-'
+    "T"
+  elsif char == '..-'
+    "U"
+  elsif char == '...-'
+    "V"
+  elsif char == '.--'
+    "W"
+  elsif char == '-..-'
+    "X"
+  elsif char == '-.--'
+    "Y"
+  elsif char == '--..'
+    "Z"
+  else
+    "Invalide morse code"
+  end
+end
+
+# for testing the decode_char method
+puts(decode_char(".-"))
+
+# decode a word
+def decode_word(word)
+  decoded = ""
+  word.split(' ').each do
+    |letter|
+    decoded += decode_char(letter)
+  end
+  decoded
+end
+
+# for testing the decode_word method
+puts decode_word("-- -.--")
