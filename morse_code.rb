7bc45ea1eec501b12c39d76672ea608adadc7e93
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-18 17:58:21 +0100

    Fix linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,58 +1,16 @@
 # decode one character
 def decode_char(char)
-  if char == '.-'
-    'A'
-  elsif char == '-...'
-    'B'
-  elsif char == '-.-.'
-    'C'
-  elsif char == '-..'
-    'D'
-  elsif char == '.'
-    'E'
-  elsif char == '..-.'
-    'F'
-  elsif char == '--.'
-    'G'
-  elsif char == '....'
-    'H'
-  elsif char == '..'
-    'I'
-  elsif char == '.---'
-    'J'
-  elsif char == '-.-'
-    'K'
-  elsif char == '.-..'
-    'L'
-  elsif char == '--'
-    'M'
-  elsif char == '-.'
-    'N'
-  elsif char == '---'
-    'O'
-  elsif char == '.--.'
-    'P'
-  elsif char == '--.-'
-    'Q'
-  elsif char == '.-.'
-    'R'
-  elsif char == '...'
-    'S'
-  elsif char == '-'
-    'T'
-  elsif char == '..-'
-    'U'
-  elsif char == '...-'
-    'V'
-  elsif char == '.--'
-    'W'
-  elsif char == '-..-'
-    'X'
-  elsif char == '-.--'
-    'Y'
-  elsif char == '--..'
-    'Z'
-  end
+  codes = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
+
+  codes[char]
 end
 
 # for testing the decode_char method
@@ -61,8 +19,7 @@ puts(decode_char('.-'))
 # decode a word
 def decode_word(word)
   decoded = ''
-  word.split.each do
-    |letter|
+  word.split.each do |letter|
     decoded += decode_char(letter)
   end
   decoded
@@ -74,14 +31,12 @@ puts decode_word('-- -.--')
 # decode the entire message
 def decode(word)
   decoded = ''
-  word.split('  ').each do
-    |words|
+  word.split('  ').each do |words|
     decoded += "#{decode_word(words)} "
   end
   decoded
 end
-  
+
 # for testing the decode method
 puts decode('-- -.--   -. .- -- .')
- puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
